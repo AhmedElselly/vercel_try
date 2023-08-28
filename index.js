c015a4b82c643e389ef5944f2d571d239d323507
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-08-28 14:14:24 +0300

    adjusted index.js

--- index.js
+++ index.js
@@ -40,62 +40,6 @@ app.get("/", (req, res) => {
 });
 
 
-app.post("/register", async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            email,
-            password: hashedPassword,
-            username: email,
-        });
-        await newUser.save();
-        return res.status(201).json({ message: "User registered successfully" });
-    } catch (error) {
-        return res.status(500).json({ error: "An error occurred" });
-    }
-});
-
-
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ error: "User not found" });
-        }
-
-        const passwordMatch = bcrypt.compareSync(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: "Invalid credentials" });
-        }
-
-        const token = jwt.sign({ userId: user._id }, secretKey, {
-            expiresIn: "1h",
-        });
-        return res.status(200).json({ token, success: true, message: "success" });
-    } catch (error) {
-        return res.status(500).json({ error: error });
-    }
-});
-
-function authenticateToken(req, res, next) {
-    const token = req.header("Authorization")?.split(" ")[1];
-    if (!token) return res.status(401).json({ error: "Access denied" });
-
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) return res.status(403).json({ error: "Invalid token" });
-
-        req.user = user;
-        next();
-    });
-}
-
-app.get("/protected", authenticateToken, (req, res) => {
-    // This route is protected and can only be accessed with a valid token
-    res.json({ message: "Protected route accessed successfully" });
-});
 
 app.listen(5000, () => {
     console.log("Running on port 5000.");
